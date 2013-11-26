6c187366f389ad9ebcb700c5f9620b5f7be7dd1f
Thirdzzz <laohanzzz@163.com>
2013-11-26 21:02:00 +0800

    修改UserInfo

--- src/yiyierliu/UserInfo.java
+++ src/yiyierliu/UserInfo.java
@@ -4,29 +4,6 @@ package yiyierliu;
 import java.util.List;
 
 public class UserInfo {
-	public int getTotalPage(List ga,int pageSize) {
-
-       
-           int  pages = ((ga.size() - 1) / pageSize + 1);
-           
-        
-        return pages;
-    }
 	
-public int getPagedRs(int pageSize, int pageNumber) {
-    	
-        
-            
-        int absoluteLocation = pageSize * (pageNumber - 1);
-
-       
-
-            //让结果集rs定位到本页之前的最后一个结果处
-      
-        
-        // 返回该页要显示的结果
-        return absoluteLocation+1;
-
-    }
 
 }
